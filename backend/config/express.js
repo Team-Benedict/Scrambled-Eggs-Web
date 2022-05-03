2364ee3c16a8e72e64212b75394bc619cda190fa
amontanooo <albert.montano711@hotmail.com>
2022-05-03 11:44:46 -0400

    Add Eslint and Prettier to the backend

--- backend/config/express.js
+++ backend/config/express.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const indexRouter = require('../src/routes/index.route.js');
-const app = express();
+const indexRouter = require('../src/routes/index.route.js')
+const app = express()
 
-app.use('/', indexRouter);
+app.use('/', indexRouter)
 
-module.exports = app;
+module.exports = app
--- backend/src/routes/tests/index.test.js
+++ backend/src/routes/tests/index.test.js
@@ -1,15 +1,12 @@
-const request = require('supertest');
-const app = require('../../../config/express.js');
+const request = require('supertest')
+const app = require('../../../config/express.js')
 
 describe('GET route responds and has expected content', () => {
-    test('route should be / and return "Eggs are being scrambled!"', done => {
+    test('route should be / and return "Eggs are being scrambled!"', (done) => {
         request(app)
             .get('/')
             .send()
-            .expect("Eggs are being scrambled!")
-            .end(function(err, res){
-                if (err) throw err;
-                else done();
-            });
-    });
-});
+            .expect('Eggs are being scrambled!')
+            .expect(200, done)
+    })
+})
