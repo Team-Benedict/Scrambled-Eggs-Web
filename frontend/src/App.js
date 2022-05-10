2d3b5de324554f89c88acf79e53419b715e96f25
amontanooo <albert.montano711@hotmail.com>
2022-05-10 14:55:17 -0400

    Resolve PR comments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,14 +5,7 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <GameInfo
-        date="February 25, 2022"
-        publish="Bandai Namco"
-        developer="FromSoftware Inc."
-        title="Elden ring"
-        platforms={["PC", "XBox", "PS5"]}
-        genres="Action Role-playing"
-      />
+      <GameInfo />
     </div>
   );
 }
--- frontend/src/Components/GameInfo/GameInfo.js
+++ frontend/src/Components/GameInfo/GameInfo.js
@@ -3,20 +3,21 @@ import PropTypes from "prop-types";
 import "./GameInfo.scss";
 
 function GameInfo({ date, publish, developer, title, platforms, genres }) {
-  const listPlatforms = () => platforms.map((platform) => <li>{platform}</li>);
+  const listItems = (item) =>
+    item.map((items) => <li key={items.toString()}>{items}</li>);
   return (
     <div className="game-info">
-      <h2 className="ttl">{title}</h2>
+      <h2 className="game-info__title">{title}</h2>
       <h3>Release date</h3>
-      <p className="rDate">{date}</p>
+      <p className="game-info__rDate">{date}</p>
       <h3>Publisher</h3>
-      <p className="pblshr">{publish}</p>
+      <p className="game-info__publisher">{publish}</p>
       <h3>Developer</h3>
-      <p className="dvlpr">{developer}</p>
+      <p className="game-info__developer">{developer}</p>
       <h3>Platforms</h3>
-      <u1 className="pltfrms">{listPlatforms()}</u1>
+      <ul className="game-info__platforms">{listItems(platforms)}</ul>
       <h3>Genres</h3>
-      <p className="gnrs">{genres}</p>
+      <ul className="game-info__genres">{listItems(genres)}</ul>
     </div>
   );
 }
@@ -26,10 +27,9 @@ GameInfo.propTypes = {
   publish: PropTypes.string.isRequired,
   developer: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  platforms: PropTypes.arrayOf(
-    PropTypes.oneOfType([PropTypes.number, PropTypes.string])
-  ).isRequired,
-  genres: PropTypes.string.isRequired,
+  platforms: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.string]))
+    .isRequired,
+  genres: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.string])).isRequired,
 };
 
 export default GameInfo;
