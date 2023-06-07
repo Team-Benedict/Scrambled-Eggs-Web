9f9f63d35fb9d247db888da88e58567c9f13d99d
Eros <erosdipede@gmail.com>
2023-06-06 21:26:54 -0400

    Add basic method for executing game creation
    - Render game list through state, which is updated on mutation

--- src/server/api/routers/game.ts
+++ src/server/api/routers/game.ts
@@ -3,5 +3,16 @@ import { z } from 'zod';
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const gameRouter = createTRPCRouter({
-    getAll: publicProcedure.query(({ctx}) => {return ctx.prisma.game.findMany()})
+    getAll: publicProcedure
+                .query(({ctx}) => {return ctx.prisma.game.findMany()}),
+    create: publicProcedure
+                .input(z.object({ name: z.string() }))
+                .mutation(async (opts) => {
+                    const { input } = opts;
+
+                    // Create a new game in the database
+                    const game = await opts.ctx.prisma.game.create({data: input});
+
+                    return game;
+                })
 })
